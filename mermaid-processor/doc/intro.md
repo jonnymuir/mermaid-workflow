e149240fe7c1f8da84028c106755a53673db8386
Jonny Muir <jonnypmuir@gmail.com>
2023-10-13 09:18:40 +0100

    feat: Initialize new project mermaid-processor
    - Create a basic project structure
    - Add initial configuration files
    - Set up basic dependencies

---
+++ mermaid-processor/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to mermaid-processor
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ mermaid-processor/project.clj
@@ -0,0 +1,10 @@
+(defproject mermaid-processor "0.1.0-SNAPSHOT"
+  :description "A project which creates a runable version of a mermaid diagram"
+  :url "https://github.com/jonnymoo/mermaid-workflow/mermaid-processor"
+  :license {:name "MIT License"
+            :url "https://github.com/jonnymoo/mermaid-workflow/blob/main/LICENSE"}
+  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :main ^:skip-aot mermaid-processor.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
