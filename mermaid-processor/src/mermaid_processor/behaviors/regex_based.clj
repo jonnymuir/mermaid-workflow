34851caf5d8934812ad5eaa04eec704291a39cc8
Jonny Muir <jonnypmuir@gmail.com>
2023-10-29 08:26:39 +0000

    fix: ensure routes are added to missing list and simplify action validation

--- mermaid-processor/src/mermaid_processor/behaviors/regex_based.clj
+++ mermaid-processor/src/mermaid_processor/behaviors/regex_based.clj
@@ -48,38 +48,28 @@
             (get-implementation-fn (:action row) match)))
         regex-to-action-map))
 
-(defn process-node-or-route [node-or-route]
-  ;; if a node - return node-text.
-  ;; if a route - return route-text.
-  ;; also return the routes.
-  (if (vector? node-or-route)
-    {:text ((second node-or-route) :node-text) :routes ((second node-or-route) :routes)}
-    {:text (node-or-route :route-destination) :routes nil}))
+(defn extract-texts [nodes]
+  (distinct
+   (concat
+    (map :node-text (vals nodes))
+    (mapcat (comp (partial map :route-text) :routes) (vals nodes)))))
 
-(defn process-nodes-or-routes [nodes-or-routes regex-to-action-map]
+
+(defn process-nodes [nodes regex-to-action-map]
   (reduce
-   (fn [[cache missing] node-or-route]
-     (let [{text :text, routes :routes} (process-node-or-route node-or-route) 
-           new-cache-missing
-           (if (cache text)
-             [cache missing]
-             (let [matching-action-fn (find-matching-action-fn regex-to-action-map text)]
-               (if matching-action-fn
-                 [(assoc cache text matching-action-fn) missing] ; add to cache if there's a match
-                 [cache (conj missing node-or-route)])))] ; add to missing if there's no match 
-       ;; If we have routes then go and process these and add them to our cache / missing
-       ;; otherwise return what we have
-       (if routes 
-         (vec (concat new-cache-missing (process-nodes-or-routes routes regex-to-action-map)))
-         new-cache-missing)))       
+   (fn [[cache missing] text]
+     (let [matching-action-fn (find-matching-action-fn regex-to-action-map text)]
+       (if matching-action-fn
+         [(assoc cache text matching-action-fn) missing] ; add to cache if there's a match
+         [cache (conj missing text)]))) ; add to missing if there's no match 
    [{} []]
-   nodes-or-routes))
+   (extract-texts nodes)))
 
 
 (defn build [chart regex-to-action-map]
   ;; Build up a cache here (so we don't have to check every regex for every command)
   ;; Also collect any actions that are missing in the chart and throw an exception
-  (let [[cache missing] (process-nodes-or-routes (:nodes chart) regex-to-action-map)]
+  (let [[cache missing] (process-nodes (chart :nodes) regex-to-action-map)]
     (when (seq missing)
       (throw (ex-info "Some nodes or routes did not match any actions" {:missing missing}))) 
     (fn [command]
--- mermaid-processor/test/mermaid_processor/behaviors/regex_based_test.clj
+++ mermaid-processor/test/mermaid_processor/behaviors/regex_based_test.clj
@@ -1,6 +1,7 @@
 (ns mermaid-processor.behaviors.regex_based_test
   (:require [clojure.test :refer :all]
-            [mermaid-processor.behaviors.regex_based :as behavior]))
+            [mermaid-processor.behaviors.regex_based :as behavior]
+            [mermaid-processor.parse :as parse]))
 
 (deftest simple-action-test
   (testing "setting a field"
@@ -20,3 +21,104 @@
              :action [:compare :score :%1 :%2]}])
           result ((behaviors "score > 5") {:fields {:score 10}})]
       (is (result :result)))))
+
+(deftest action-missing-test
+  (testing "valdate an action isnt present"
+    (let [chart (parse/parse-mermaid "flowchart TD
+                                      A[Score 10]")
+          ex (try
+               (behavior/build chart {})
+               (catch clojure.lang.ExceptionInfo e e))]
+      (is (= {:missing ["Score 10"]}
+             (ex-data ex))))))
+
+(deftest condition-missing-test
+  (testing "valdate a condition isnt present"
+    (let [chart (parse/parse-mermaid "flowchart TD
+                                      A-->|MyTest|B")
+          ex (try
+               (behavior/build chart {})
+               (catch clojure.lang.ExceptionInfo e e))]
+      (is (= ((set ["A" "B" "MyTest"])
+             (set ((ex-data ex) :missing))))))))
+
+(deftest complex-chart-test
+  (testing "valdate a more complex chart"
+    (let [chart (parse/parse-mermaid "flowchart TD
+                                     Q1{Any Red Circles}
+                                         Q1 --> |Yes| Q2
+                                         Q1 --> |No| O3a
+                                         Q2{Only blue circles?}
+                                         Q2 --> |Yes| Q3
+                                         Q2 --> |No| Q4
+                                         Q3{Radius larger than 50?}
+                                         Q3 --> |Yes| Q5
+                                         Q3 --> |No | O3b
+                                         Q4{Any rectangles or squares?}
+                                         Q4 --> |Yes| Q6
+                                         Q4 --> |No| Q7
+                                         Q5{Radius larger than 100?}
+                                         Q5 --> |Yes| O1c
+                                         Q5 --> |No| O2c
+                                         Q6{Any text?}
+                                         Q6 --> |Yes| Q8
+                                         Q6 --> |No | Q10
+                                         Q7{Any text?}
+                                         Q7 --> |Yes| Q9
+                                         Q7 --> |No | O2d
+                                         Q8{Rectangle green?}
+                                         Q8 --> |Yes| O1e
+                                         Q8 --> |No| Q10
+                                         Q9{Text containing the sequence 'Lhasa'?}
+                                         Q9 --> |Yes| O1f
+                                         Q9 --> |No| O3f
+                                         Q10{More than one element in the file?}
+                                         Q10 --> |No| O1e
+                                         Q10 --> |Yes| Q11
+                                         Q11{Any straight lines?}
+                                         Q11 --> |Yes| Q12
+                                         Q11 --> |No| Q13
+                                         Q12{Every line longer than 100?}
+                                         Q12 --> |Yes| O2g
+                                         Q12 --> |No| O3g
+                                         Q13{Any ellipses?}
+                                         Q13 --> |Yes| Q14
+                                         Q13 --> |Yes| Q15
+                                         Q14{Any ellipse with height >=50?}
+                                         Q14 --> |Yes| O3h
+                                         Q14 --> |No| Q16
+                                         Q15{Any elements with opacity less than 1?}
+                                         Q15 --> |Yes| Q17
+                                         Q15 --> |No| O3i
+                                         Q16{Any rectangle with area >= 300?}
+                                         Q16 --> |Yes| O1j
+                                         Q16 --> |No| Q18
+                                         Q17{Total element count > 5?}
+                                         Q17 --> |Yes| O1k
+                                         Q17 --> |No| O2k
+                                         Q18{Total element count > 5?}
+                                         Q18 --> |Yes| O2l
+                                         Q18 --> |No| O3l
+                                         O3a[Score 3]
+                                         O3b[Score 3]
+                                         O1c[Score 1]
+                                         O2c[Score 2]
+                                         O2d[Score 2]
+                                         O1e[Score 1]
+                                         O1f[Score 1]
+                                         O3f[Score 3]
+                                         O2g[Score 2]
+                                         O3g[Score 3]
+                                         O3h[Score 3]
+                                         O3i[Score 3]
+                                         O1j[Score 1]
+                                         O1k[Score 1]
+                                         O2k[Score 2]
+                                         O2l[Score 2]
+                                         O3l[Score 3]")
+          ex (try
+               (behavior/build chart {})
+               (catch clojure.lang.ExceptionInfo e e))]
+      (is (= (set ["Any ellipse with height >=50?" "Score 1" "Score 2" "Any ellipses?" "Any rectangle with area >= 300?" "Score 3" "Any straight lines?" "Radius larger than 100?" "Radius larger than 50?" "Only blue circles?" "Any text?" "Any Red Circles" "Total element count > 5?" "Any rectangles or squares?" "Text containing the sequence 'Lhasa'?" "Any elements with opacity less than 1?" "More than one element in the file?" "Every line longer than 100?" "Rectangle green?" "Yes" "No"])
+             (set ((ex-data ex) :missing))))
+      (is (= 21 (count ((ex-data ex) :missing)))))))
