9c4867086b825400315cbdc85a11450c79d57164
Jonny Muir <jonnypmuir@gmail.com>
2023-11-02 13:31:48 +0000

    ci: remove clj-kondo linting from GitHub Actions workflow

--- mermaid-processor/project.clj
+++ mermaid-processor/project.clj
@@ -1,6 +1,7 @@
 (defproject mermaid-processor "0.1.0-SNAPSHOT"
   :description "A project which creates a runable version of a mermaid diagram"
   :url "https://github.com/jonnymoo/mermaid-workflow/mermaid-processor"
+  :scm "https://github.com/jonnymoo/mermaid-workflow/tree/main/mermaid-processor/src"
   :license {:name "MIT License"
             :url "https://github.com/jonnymoo/mermaid-workflow/blob/main/LICENSE"}
   :dependencies [[org.clojure/clojure "1.11.1"]
