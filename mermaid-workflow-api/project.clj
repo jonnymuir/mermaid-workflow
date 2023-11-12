dff3d19026e54cba44c52827ab6eda63ead1a6e3
Jonny Muir <jonnypmuir@gmail.com>
2023-11-12 19:55:53 +0000

    chore: project version increase.

--- mermaid-workflow-api/project.clj
+++ mermaid-workflow-api/project.clj
@@ -11,7 +11,7 @@
                  [metosin/ring-swagger-ui "5.9.0"]
                  [clj-http "3.12.3"]
                  [cheshire "5.10.0"]
-                 [com.jonnymuir/mermaid-processor "0.1.15-SNAPSHOT"]]
+                 [com.jonnymuir/mermaid-processor "0.1.16-SNAPSHOT"]]
   
   :repl-options {:init-ns mermaid-workflow-api.server}
   :main mermaid-workflow-api.core
